a37f9e683f497fd8f6d135128a5be9823191dc4b
Jonathan Gamble <jdgamble555@gmail.com>
2025-10-09 19:52:33 -0500

    externals sharp

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   build: {
-    target: ['es2020'],
-    rollupOptions: {
-      external: ['sharp', '@img/sharp-wasm32', '@img/sharp-linux-x64']
-    },
+    target: ['es2020']
   },
   resolve: {
     mainFields: ['module'],
@@ -23,6 +20,9 @@ export default defineConfig(() => ({
         routes: [],
       },
       nitro: {
+        externals: {
+          external: ['sharp', '@img/sharp-wasm32', '@img/sharp-linux-x64']
+        },
         preset: 'vercel-edge'
       }
     }),
